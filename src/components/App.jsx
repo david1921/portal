7b08ff8867be35997b81afc152e1bbcaa1452724
David Mbura <David.Mbura@bhnetwork.com>
2017-08-10 00:08:52 -0700

    elastic beanstalk files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ export default class App extends React.Component {
 	render(){
 		console.log("hellooo")
 		return(<div className="header">
-            Hello World!!! Number 3
+            Hello World!!! Number 5
          </div>) 
 	}
 }
